8620e03e207c59f5977271eb0741dc75fe807c91
Mikael Lindqvist <li.mikael@gmail.com>
2023-10-29 15:46:58 +0800

    router example

--- examples/router/src/index.jsx
+++ examples/router/src/index.jsx
@@ -1,17 +1,41 @@
-import {useIsoMemo} from "isoq";
-import {useState} from "react";
+import {Link, Route, useLoaderData} from "isoq";
+
+function Home() {
+	return (
+		<p>This is the home page</p>
+	);
+}
+
+function SomePage() {
+	let loaderData=useLoaderData();
+
+	return (
+		<p>This is another page, loaderData={loaderData}</p>
+	);
+}
+
+SomePage.loader=async()=>{
+	console.log("Loading some data for some page...");
+
+	await new Promise(r=>setTimeout(r,1000));
+
+	return "123";
+}
 
 export default function Main() {
-	let [value,setValue]=useState(1);
-	let doubleValue=useIsoMemo(async()=>{
-		console.log("the value is: "+value);
-		return value*2;
-	},[value]);
-
 	return (<>
-		<h1>Hello World</h1>
-		<p>This is the website...</p>
-		<p>Double value is: {doubleValue}</p>
-		<button onclick={()=>setValue(value+1)}>inc</button>
+		<div>
+			<Link href="/">Home</Link>
+			|
+			<Link href="/somepage">Some Page</Link>
+		</div>
+
+		<Route path="/">
+			<Home/>
+		</Route>
+
+		<Route path="/somepage" loader={SomePage.loader}>
+			<SomePage/>
+		</Route>
 	</>);
 }
